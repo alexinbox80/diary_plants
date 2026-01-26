5a4c6c5a3b7f09e6e9c3d9489a7c4e98d980c759
AlexeySerov <alexinbox80@inbox.ru>
2026-01-26 18:08:52 +0300

    Make some change in DeleteImage Manager and modal_delete template twig

--- src/Controller/Web/Admin/Image/DeleteImage/Manager.php
+++ src/Controller/Web/Admin/Image/DeleteImage/Manager.php
@@ -3,19 +3,28 @@
 namespace App\Controller\Web\Admin\Image\DeleteImage;
 
 use App\Domain\Service\AttachmentService;
+use App\Domain\Service\FileService;
 use Symfony\Component\HttpFoundation\Request;
 
 class Manager
 {
     public function __construct(
         private readonly AttachmentService $attachmentService,
+        private readonly FileService $fileService
     ) {
 
     }
 
     public function deleteData(int $id, Request $request): array
     {
-        $this->attachmentService->removeById($id);
+        $attachment = $this->attachmentService->find($id);
+
+        if (!is_null($attachment->getPath()) || !is_null($attachment->getFilename())) {
+            $this->fileService->removeUploadedFile($attachment->getPath(). $attachment->getFilename());
+            $this->attachmentService->removeAttachment($attachment);
+        } else {
+            $this->attachmentService->removeById($id);
+        }
 
         $request->getSession()->getFlashBag()->add('success', 'Изображение успешно удалено.');
         return ['success' => true];
